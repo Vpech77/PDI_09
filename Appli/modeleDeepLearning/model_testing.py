c1dcf398a6b28374a947b96ea10f10e54974267e
Vpech77 <vanessa.pech77@gmail.com>
2024-05-14 17:36:17 +0200

    code test clean

--- Appli/modeleDeepLearning/model_testing.py
+++ Appli/modeleDeepLearning/model_testing.py
@@ -1,23 +1,10 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
 import keras
-from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, BatchNormalization, UpSampling2D
-from keras.layers import  Dropout, Activation
-from keras.optimizers import Adam, SGD
-from keras.layers import LeakyReLU
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from keras import backend as K
-from keras.utils import plot_model
 import tensorflow as tf
-import glob
-import random
-from random import shuffle
-import sys
-np.set_printoptions(threshold=sys.maxsize)
 
 def mean_iou(y_true, y_pred):
     '''
@@ -41,42 +28,37 @@ def mean_iou(y_true, y_pred):
     return iou
 
 MODEL_NAME = 'unet_32batch_30epoch'
-IMG_TEST_FOLDER = '2006'
+IMG_TEST_NAME = '2006'
+
+PATH_IMG_TEST = "./imgCartesAnciennes/" + IMG_TEST_NAME
+PATH_MODEL_FILE = "./model/" + MODEL_NAME + ".keras"
 
 if __name__ == "__main__":
 
-    dossier_git = 'model'
+    model = keras.models.load_model(PATH_MODEL_FILE, custom_objects={"mean_iou": mean_iou})
+    test = os.listdir(PATH_IMG_TEST)
 
-    if os.path.isdir(dossier_git):
-        contenu_dossier = os.listdir(dossier_git)
-        print(contenu_dossier)
-    else:
-        print("nope")
-
-    # model = keras.models.load_model('unet_32batch_30epoch.keras', custom_objects={"mean_iou": mean_iou})
-    test = os.listdir(IMG_TEST_FOLDER)
-    # i=1
+    i=1
     for image in test :
-        path = os.path.join("./imgCartesAnciennes"+IMG_TEST_FOLDER, image)
+        path = os.path.join(PATH_IMG_TEST, image)
         raw = Image.open(path)
         raw = np.array(raw.resize((256, 256)))/255.
         raw = raw[:,:,0:3]
-        print(raw)
 
-    #     #predict the mask
-    #     pred = model.predict(np.expand_dims(raw, 0))
+        #predict the mask
+        pred = model.predict(np.expand_dims(raw, 0))
 
-    #     #mask post-processing
-    #     msk  = pred.squeeze()
-    #     msk = np.stack((msk,)*3, axis=-1)
-    #     msk[msk >= 0.5] = 1
-    #     msk[msk < 0.5] = 0
+        #mask post-processing
+        msk  = pred.squeeze()
+        msk = np.stack((msk,)*3, axis=-1)
+        msk[msk >= 0.5] = 1
+        msk[msk < 0.5] = 0
 
-    #     #show the mask and the segmented image
-    #     combined = np.concatenate([raw, msk, raw* msk], axis = 1)
-    #     plt.axis('off')
-    #     plt.imshow(combined)
-    #     plt.savefig('./imgCartesAnciennes/'+MODEL_NAME+str(i)+'.png')
-    #     #plt.show()
-    #     i+=1
+        #show the mask and the segmented image
+        combined = np.concatenate([raw, msk, raw* msk], axis = 1)
+        plt.axis('off')
+        plt.imshow(combined)
+        plt.savefig('./output_testing/'+MODEL_NAME+str(i)+'.png')
+        #plt.show()
+        i+=1
 
